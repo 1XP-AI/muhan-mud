675d946c57d36db39015f43356623bfbb556180b
JinWoo Lee <jjangg96@gmail.com>
2026-09-07 22:53:49 +0900

    test: require live bank snapshot to include equipped inventory

--- tests/unit/bank_money_live_snapshot_test.c
+++ tests/unit/bank_money_live_snapshot_test.c
@@ -22,6 +22,21 @@ int main(int argc,char **argv)
     assert(bank_money_live_snapshot(player,&changed,&changed_length)==0);
     assert(changed_length==size && memcmp(changed,actual,size));
     free(changed); free(actual);
+    {
+        creature equipped,normalized,saved_player; object a,z,saved_z; otag ta,tz;
+        unsigned char *wanted=NULL,*got=NULL; size_t wanted_length=0,got_length=0;
+        memset(&equipped,0,sizeof(equipped));equipped.type=PLAYER;strcpy(equipped.name,"Gearhero");
+        memset(&a,0,sizeof(a));memset(&z,0,sizeof(z));strcpy(a.name,"A");strcpy(z.name,"Z");
+        memset(&ta,0,sizeof(ta));memset(&tz,0,sizeof(tz));ta.obj=&a;tz.obj=&z;
+        equipped.first_obj=&ta;equipped.ready[0]=&z;normalized=equipped;
+        normalized.ready[0]=NULL;ta.next_tag=&tz;
+        assert(!player_snapshot_v1_encode_loaded(&normalized,&wanted,&wanted_length));
+        ta.next_tag=NULL;saved_player=equipped;saved_z=z;
+        assert(!bank_money_live_snapshot(&equipped,&got,&got_length));
+        assert(got_length==wanted_length&&!memcmp(got,wanted,got_length));
+        assert(!memcmp(&equipped,&saved_player,sizeof(equipped))&&!memcmp(&z,&saved_z,sizeof(z))&&ta.next_tag==NULL);
+        free(wanted);free(got);
+    }
     player->type=MONSTER; actual=(unsigned char *)1; size=1;
     assert(bank_money_live_snapshot(player,&actual,&size)!=0 && !actual && !size);
     player_snapshot_v1_free_clone(player); return 0;
